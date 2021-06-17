2e32086d9e5b9150c0df75e081a2ff0704f3cdcd
Amy Chen <amy@fishtownanalytics.com>
2021-06-17 17:37:34 +0000

    commit demo

--- models/marts/airbnb/core/dim_neighborhoods.sql
+++ models/marts/airbnb/core/dim_neighborhoods.sql
@@ -1,9 +1,41 @@
 {{ config(materialized='table') }}
-
-with source as (
-
+ 
+with neighborhoods as (
+ 
     select * from {{ ref('stg_airbnb_neighborhoods') }}
-
+ 
+),
+ 
+listings as (
+ 
+  select * from {{ref('stg_airbnb_listings')}}
+ 
+),
+ 
+agg_listings as (
+ 
+  select 
+ 
+  neighborhood_cleansed,
+  count(listing_id) as total_listings
+ 
+from listings 
+group by 1
+ 
+),
+ 
+joined as (
+ 
+  select 
+ 
+  neighborhoods.*,
+  total_listings
+ 
+  from neighborhoods
+  left join agg_listings 
+  on neighborhoods.neighborhood_name = agg_listings.neighborhood_cleansed
+ 
 )
-
-select * from source
+ 
+ 
+select * from joined
