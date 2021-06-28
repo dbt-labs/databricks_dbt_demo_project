ae333160f05bf272f6e63328c828b5ea0dee9290
Jon Natkins <jon.natkins@fishtownanalytics.com>
2021-06-28 18:48:43 +0000

    remove a couple broken things

--- macros/optimize.sql
+++
@@ -1,4 +0,0 @@
-{% macro set_auto_optimize() %}
-  set spark.databricks.delta.properties.defaults.autoOptimize.optimizeWrite = true;
-  set spark.databricks.delta.properties.defaults.autoOptimize.autoCompact = true;
-{% endmacro %}
