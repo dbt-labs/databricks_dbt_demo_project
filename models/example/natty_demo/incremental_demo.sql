f11a51dfaaf74731ef66784548eafd23dca1bf07
Jonathan Natkins <natty@dbtlabs.com>
2021-10-08 16:53:17 +0000

    delta format

--- models/example/natty_demo/incremental_demo.sql
+++ models/example/natty_demo/incremental_demo.sql
@@ -1,7 +1,6 @@
 {{ config(
     materialized = 'incremental',
-    unique_key='id',
-    pre_hook='SET spark.databricks.delta.schema.autoMerge.enabled=true')
+    file_format = 'delta')
 }}
 
 select id, date_day
