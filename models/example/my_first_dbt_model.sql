2937e0110a31c34ada237a255f29662cdf7ea1a1
sung.won.chung@fishtownanalytics.com <sung.won.chung@fishtownanalytics.com>
2021-11-23 17:32:14 +0000

    update my first dbt model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,13 +7,13 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 with source_data as (
 
     select 1 as id
-    -- union all
-    -- select null as id
+    union all
+    select null as id
 
 )
 
