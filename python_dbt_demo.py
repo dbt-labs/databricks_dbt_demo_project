0edab7dee40a32ddfa7138ae65a960c9a70e1e64
Sung Won Chung <sungwonchung3@gmail.com>
2022-05-05 11:12:19 -0500

    fal flow run works

--- python_dbt_demo.py
+++ python_dbt_demo.py
@@ -2,11 +2,40 @@ from fal import FalDbt
 import os
 
 cwd = os.getcwd()
-
 faldbt = FalDbt(profiles_dir=cwd, project_dir=cwd)
 
-print(faldbt.list_sources())
+faldbt.list_sources()
+# [['results', 'ticket_data_sentiment_analysis']]
 
-dim_listings_df = faldbt.ref('dim_neighborhoods')
+# faldbt.list_models()
+# {
+#   'zendesk_ticket_metrics': <RunStatus.Success: 'success'>, 
+#   'stg_o3values': <RunStatus.Success: 'success'>, 
+#   'stg_zendesk_ticket_data': <RunStatus.Success: 'success'>, 
+#   'stg_counties': <RunStatus.Success: 'success'>
+# }
 
-print(dim_listings_df.tail())
+# ref() means the same thing to everyone in the data team(think: data engineers, data analysts, data scientists)
+# no need to fiddle with ad hoc SQL to get the dataframe
+sentiments = faldbt.ref('fct_reviews')
+sentiments.tail(10)
+
+# let's enrich the dataframe with the sentiment scores
+from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
+
+sentiments_sample = sentiments.tail(10)
+
+sentiments_polarity = []
+sentiments_subjectivity = []
+for index, row in sentiments_sample.iterrows():
+    text = row['comments']
+    blob = TextBlob(text)
+    sentiments_polarity.append(blob.sentiment.polarity)
+    sentiments_subjectivity.append(blob.sentiment.subjectivity)
+
+
+sentiments_sample['polarity'] = sentiments_polarity
+sentiments_sample['subjectivity'] = sentiments_subjectivity
+
+print(sentiments_sample)
