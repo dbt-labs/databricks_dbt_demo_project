3a7a0112bc3a68359f8298324466a6a63e52b835
Gordon Rose <gordon.rose@fishtownanalytics.com>
2022-09-23 18:48:03 +0000

    Made a change

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -2,13 +2,13 @@
 select 
     1 as id,
     2 as sale_amount,
-    3 / 4 as payment_amount
+    (3 / 4) * 100 as payment_amount
 
 union all
 
 select 
     2 as id,
     5 as sale_amount,
-    1 / 3 as payment_amount
+    (1 / 3) * 100 as payment_amount
    
 
