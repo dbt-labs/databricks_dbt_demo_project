e5228a27d96172f5761b05dfc456c4776e7534e3
Harsh Jetly <harsh.jetly@fishtownanalytics.com>
2022-09-26 16:56:57 +0000

    added loyalty model

--- models/staging/tpch/stg_tpch_loyaltycustomers.sql
+++ models/staging/tpch/stg_tpch_loyaltycustomers.sql
@@ -12,7 +12,6 @@ renamed as (
         c_name as name,
         c_address as address, 
         c_nationkey as nation_key,
-        c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
         c_comment as comment
