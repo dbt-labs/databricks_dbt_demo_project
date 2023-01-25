028e6a33dce13d0a8301d41704f1f3c13ea6f190
Ernesto Ongaro <ernesto.ongaro@fishtownanalytics.com>
2023-01-25 12:19:23 +0000

    fixes this for valeria and josh

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -9,7 +9,7 @@ renamed as (
 
     select
     
-        o_orderkey as order_key,
+        o_orderkey as order_key_lego,
         o_custkey as customer_key,
         o_orderstatus as status_code,
         o_totalprice as total_price,
