119f4e5d314897c4269e103410a49c193698217f
isabela.sobral@dbtlabs.com <isabela.sobral@dbtlabs.com>
2023-03-30 21:10:18 +0000

    deleted stagin column

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -14,7 +14,6 @@ renamed as (
                 as order_item_key,
         l_orderkey as order_key,
         l_partkey as part_key,
-        l_suppkey as supplier_key,
         l_linenumber as line_number,
         l_quantity as quantity,
         l_extendedprice as extended_price,
