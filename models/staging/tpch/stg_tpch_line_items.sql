84c54465ac924f6dfcfc388d610a69d20b6e52fe
Mark <mark.wan@dbtlabs.com>
2023-06-01 02:09:04 +0000

    Updated documentation and packages

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -20,8 +20,21 @@ renamed as (
         l_extendedprice as extended_price,
         l_discount as discount_percentage,
         l_tax as tax_rate,
-        l_returnflag as return_flag,
-        l_linestatus as status_code,
+
+        case l_returnflag
+            when 'R' then 'returned'
+            when 'N' then 'normal'
+            when 'A' then 'awaiting return'
+            else null
+        end as return_flag, 
+
+        case l_linestatus 
+            when 'P' then 'returned'
+            when 'F' then 'billed'
+            when 'O' then 'shipped'
+            else null
+        end as status_code,
+        
         l_shipdate as ship_date,
         l_commitdate as commit_date,
         l_receiptdate as receipt_date,
