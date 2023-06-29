8923bf62dc4363cab0115c1b6110ee65667924c7
isabela.sobral@dbtlabs.com <isabela.sobral@dbtlabs.com>
2023-06-29 22:16:56 +0000

    adjusted stg_orers

--- models/staging/tpch/stg_tpch_orders.sql
+++ models/staging/tpch/stg_tpch_orders.sql
@@ -9,11 +9,7 @@ renamed as (
 
     select
     
-        
-        o_orderpriority as priority_code,
-        o_clerk as clerk_name,
-        o_shippriority as ship_priority,
-        o_comment as comment
+
 
     from source
 
