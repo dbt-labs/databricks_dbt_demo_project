aab47366a11e3ba30989983084debed294eec59f
isabela.sobral@dbtlabs.com <isabela.sobral@dbtlabs.com>
2023-07-12 19:55:35 +0000

    adjusted

--- models/demo_schema_evolution/source_schema.sql
+++ models/demo_schema_evolution/source_schema.sql
@@ -1,19 +1,19 @@
 {{ config(materialized="table") }}
 
-select
-    1 as order_id,
-    1 as customer_id,
-    'cancelled' as status,
-    cast('2018-01-01' as date) as order_date,
-    cast('2018-01-01' as date) as modified_at
+-- select
+--     1 as order_id,
+--     1 as customer_id,
+--     'cancelled' as status,
+--     cast('2018-01-01' as date) as order_date,
+--     cast('2018-01-01' as date) as modified_at
 
 
-union select
-    2 as order_id,
-    1 as customer_id,
-    'completed' as status,
-    cast('2018-01-02' as date) as order_date,
-    cast('2018-01-02' as date) as modified_at
+-- union select
+--     2 as order_id,
+--     1 as customer_id,
+--     'completed' as status,
+--     cast('2018-01-02' as date) as order_date,
+--     cast('2018-01-02' as date) as modified_at
 
 
 -- -- -- add a new record and new column
@@ -40,27 +40,27 @@ union select
 
 
 
--- select
---     1 as order_id,
---     1 as customer_id,
---     'cancelled' as status,
---     cast('2018-01-01' as date) as order_date,
---     cast('2018-01-01' as date) as modified_at
---     ,null as city
+select
+    1 as order_id,
+    1 as customer_id,
+    'cancelled' as status,
+    cast('2018-01-01' as date) as order_date,
+    cast('2018-01-01' as date) as modified_at
+    ,null as city
 
--- union select
---     2 as order_id,
---     1 as customer_id,
---     'completed' as status,
---     cast('2018-01-02' as date) as order_date,
---     cast('2018-01-02' as date) as modified_at
---     ,null as city
+union select
+    2 as order_id,
+    1 as customer_id,
+    'completed' as status,
+    cast('2018-01-02' as date) as order_date,
+    cast('2018-01-02' as date) as modified_at
+    ,null as city
 
--- -- -- add a new record and new column
--- union select
---     3 as order_id,
---     2 as customer_id,
---     'completed' as status,
---     cast('2018-01-03' as date) as order_date,
---     cast('2018-01-03' as date) as modified_at,
---     'san francisco' as city
+-- -- add a new record and new column
+union select
+    3 as order_id,
+    2 as customer_id,
+    'completed' as status,
+    cast('2018-01-03' as date) as order_date,
+    cast('2018-01-03' as date) as modified_at,
+    'san francisco' as city
