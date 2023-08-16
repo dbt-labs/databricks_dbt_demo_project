168b360ffb935d8ae941a4fdb10ed9f15c535341
Ernesto Ongaro <ernesto.ongaro@fishtownanalytics.com>
2023-08-16 08:32:12 +0000

    fixes that thing for elina

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -21,7 +21,7 @@ region as (
 ),
 final as (
     select 
-        customer.customer_key,
+        1 as customer_key,
         customer.name,
         customer.address,
         {# nation.nation_key as nation_key, #}
