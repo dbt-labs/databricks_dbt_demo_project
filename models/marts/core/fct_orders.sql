65dd20be0a62ca8e4c3e05d5acbaba0d63950c43
Ernesto Ongaro <ernesto.ongaro@fishtownanalytics.com>
2023-10-24 08:38:16 +0000

    adds small change

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -21,7 +21,7 @@ order_item_summary as (
     select 
         order_key,
         sum(1) as my_sum,
-        sum(gross_item_sales_amount) as gross_item_sales_amount,
+        sum(gross_item_sales_amount - 0.01) as gross_item_sales_amount,
         sum(item_discount_amount) as item_discount_amount,
         sum(item_tax_amount) as item_tax_amount,
         sum(net_item_sales_amount) as net_item_sales_amount
