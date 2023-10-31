9782c0ed2f11fa3060defc5272eb26ba4e259e27
Ernesto Ongaro <ernesto.ongaro@fishtownanalytics.com>
2023-10-31 12:32:30 +0000

    adds new field

--- models/staging/tpch/stg_tpch_nations.sql
+++ models/staging/tpch/stg_tpch_nations.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='nation_key'
+    )
+}}
+
 with source as (
 
     select * from {{ source('tpch', 'nation') }}
@@ -9,6 +16,7 @@ renamed as (
     select
     
         n_nationkey as nation_key,
+        'my_field' as ernesto_field,
         n_name as name,
         n_regionkey as region_key,
         n_comment as comment
